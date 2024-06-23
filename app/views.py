aa2c372764c9347f72ef05e2ea5beacde2e9e305
aruniyappan <aruniyappan1997@gmail.com>
2024-06-23 22:08:51 +0530

    Updated the data

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def home(request):
+    data = {
+        "name": "Jhon Doe",
+        "age": "30"
+    }
+    return JsonResponse(data)
